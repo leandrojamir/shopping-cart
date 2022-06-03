ebef14c82be1294d834c1e9642e21a67c9a26600
leandrojamir <leandrojamir@hotmail.com>
2022-06-03 18:03:42 -0300

    adcionando fetchItem para complementar teste 3

--- helpers/fetchItem.js
+++ helpers/fetchItem.js
@@ -1,6 +1,16 @@
-const fetchItem = () => {
-  // seu código aqui
+const fetchItem = async (ItemID) => {
+  try {
+    const url = `https://api.mercadolibre.com/items/${ItemID}`;
+    const response = await fetch(url);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    return error;
+  }
 };
+fetchItem('MLB1341706310');
 
 if (typeof module !== 'undefined') {
   module.exports = {
