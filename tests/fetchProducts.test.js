61ee0a49ab6ddd57cb7c75eefbd9463f0c7edf28
leandrojamir <leandrojamir@hotmail.com>
2022-06-03 15:06:59 -0300

    Adcionando item 1, teste 25% ok

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,7 +2,9 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
-describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('1 - Teste a função fecthProducts', () => {
+  // 1. (TDD) Desenvolva testes de no mínimo 25% de cobertura total e 100% da função fetchProducts
+  it('Teste se fetchProducts é uma função;', () => {
+    expect(typeof fetchProducts === 'function').toBe(true);
+  });
 });
