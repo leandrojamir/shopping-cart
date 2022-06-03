2a260496e10b44fa18b26fa16d6f0fc3f82da361
leandrojamir <leandrojamir@hotmail.com>
2022-06-03 17:46:29 -0300

    adcionando demais testes item 1 e 3

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,8 +3,22 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // 1. (TDD) Desenvolva testes de no mínimo 25% de cobertura total e 100% da função fetchProducts
   it('Teste se fetchProducts é uma função;', () => {
     expect(typeof fetchProducts === 'function').toBe(true);
+  });
+  it('Executa fetchProducts com "computador" e testa se fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  });
+  it('Ao chamar fetchProducts com "computador", ver se fetch utiliza endpoint abaixo', async () => {
+    await fetchProducts('computador');
+    const endPoint = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(endPoint);
+  });
+  it('Testa fetchProducts com "computador", se é igual ao objeto computadorSearch', async () => {
+    expect(await fetchProducts('computador')).toEqual(computadorSearch);
+ });
+  it('Ao chamar fetchProducts sem argumento, retorna um erro', async () => {
+    expect(await fetchProducts()).toEqual(new Error('You must provide an url'));
   });
 });
